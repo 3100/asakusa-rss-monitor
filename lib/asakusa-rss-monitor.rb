bb04745223d9b916971dbca6e59e44017b08ff9a
3100 <sugar16g@gmail.com>
2013-01-19 12:26:26 +0900

    Modify. Remove folder sample.

--- lib/asakusa-rss-monitor.rb
+++ lib/asakusa-rss-monitor.rb
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
-require "asakusa-rss-monitor/version"
-require "asakusa-rss-monitor/rss-monitor"
 require "asakusa-rss-monitor/as-info"
+require "asakusa-rss-monitor/date-save"
+require "asakusa-rss-monitor/date-checker"
+require "asakusa-rss-monitor/post-bot"
+require "asakusa-rss-monitor/rss-getter"
+require "asakusa-rss-monitor/rss-monitor"
+require "asakusa-rss-monitor/version"
